28c8e52f2e4319fc8bb45d099ec7ece862afc1e3
Ananth Astaputra <ananthastaputra@Ananths-MacBook-Air.local>
2024-02-15 19:46:40 +0530

    react app init

---
+++ src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Layout = () => {
+    return (
+        <>
+            <div>
+                this App is still under progress
+            </div>
+        </>
+    )
+}
+export default Layout;
